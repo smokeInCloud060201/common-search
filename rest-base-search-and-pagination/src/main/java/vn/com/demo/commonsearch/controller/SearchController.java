5fff0872be0c73d583dbfd6748150a04f79ed9ec
Ngoc Khanh <khanhk56a12@gmail.com>
2025-01-01 16:53:03 +0700

    Completed Base Search Common For Restful

--- rest-base-search-and-pagination/src/main/java/vn/com/demo/commonsearch/controller/SearchController.java
+++ rest-base-search-and-pagination/src/main/java/vn/com/demo/commonsearch/controller/SearchController.java
@@ -5,15 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.com.demo.commonsearch.search.dto.SearchRequest;
-import vn.com.demo.commonsearch.search.service.SearchService;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/search")
 public class SearchController {
-
-    private final SearchService searchService;
 
     @GetMapping
     public ResponseEntity<Object> searchWorksite(@ModelAttribute SearchRequest request) {
