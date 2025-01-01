2443c292a279252e1e2cd23ab3528f6ad6c1ad3e
Ngoc Khanh <khanhk56a12@gmail.com>
2025-01-01 20:14:44 +0700

    ReStructure project

--- graphQL/src/main/java/org/example/graphql/enums/Gender.java
+++
@@ -1,6 +0,0 @@
-package org.example.graphql.enums;
-
-public enum Gender {
-    M,
-    F
-}
--- rest-base-search-and-pagination/src/main/java/vn/com/demo/commonsearch/worksite/entities/SiteStatus.java
+++
@@ -1,5 +0,0 @@
-package vn.com.demo.commonsearch.worksite.entities;
-
-public enum SiteStatus {
-    ACTIVE, PENDING_APPROVAL, APPROVED, REJECTED
-}
