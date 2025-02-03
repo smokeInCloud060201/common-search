629f018e70195544a11245e7ea70918cc56abd85
Ngoc Khanh <khanhk56a12@gmail.com>
2025-02-03 21:08:39 +0700

    Change querydsl from deadProject to alive project

--- jpa/graphQL/src/main/java/org/example/graphql/services/QueryBuilderCustom.java
+++ jpa/graphQL/src/main/java/org/example/graphql/services/QueryBuilderCustom.java
@@ -1,13 +1,11 @@
 package org.example.graphql.services;
 
 import com.querydsl.core.types.EntityPath;
-import org.example.graphql.entity.QStudent;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.graphql.data.query.QuerydslDataFetcher;
 
 public class QueryBuilderCustom implements QuerydslBinderCustomizer<EntityPath<?>> {
-
 
     @Override
     public void customize(QuerydslBindings bindings, EntityPath<?> root) {
